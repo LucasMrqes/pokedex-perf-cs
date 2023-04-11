7b3f6494a07221f433beb9a89277782e21ae25cf
LucasMrqes <lucas.marques@student-cs.fr>
2023-04-11 10:08:21 +0200

    Improved image download perfs with correct sizes

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,21 +7,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <main className={styles.main}>
       <div className={styles.logoContainer}>
         <Link href="/" className={styles.logo}>
-          <Image
+        <Image
             src="/pokeball.png"
             alt="pokedex logo"
             width={100}
             height={100}
+            priority
           />{" "}
           Pokedex
         </Link>
       </div>
       <div className={styles.heroImageContainer}>
         <Image
-          src="/pokemon-hero-image-alt.png"
-          alt=""
-          sizes="100vw"
-        />
+            src="/pokemon-hero-image-alt.png"
+            alt=""
+            className={styles.heroBanner}
+            fill
+            priority
+          />
       </div>
       <div className={styles.content}>{children}</div>
     </main>
